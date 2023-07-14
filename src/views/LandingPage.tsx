2c38b4086b119d1a608d08c6dec20ff242a602fb
Duzie <72194611+duzzisantos@users.noreply.github.com>
2023-07-14 20:05:05 +0200

    added new page for my-services

--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -17,7 +17,6 @@ const LandingPage: FunctionComponent = () => {
   };
   return (
     <div className="col-10 px-5 pt-5 py-5 ms-auto vh-100 vstack gap-5 d-flex flex-column align-items-center">
-      Testing testing 1234
       <LandingPageScrollContainer
         content={servicesList}
         category="Services"
