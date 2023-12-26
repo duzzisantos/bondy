b494b9e5b363bf4a703b438fc34956607732333d
Duzie <duzzi4x@gmail.com>
2023-12-26 22:26:50 +0100

    cleanup

--- .history/src/views/BookService_20231226222618.tsx
+++ .history/src/views/BookService_20231226222618.tsx
@@ -37,7 +37,7 @@ const BookService: FunctionComponent = () => {
           ))}
         </Tabs>
       </div>
-      <div className="shadow-sm"></div>
+      <div className="shadow-sm">LOL</div>
     </div>
   );
 };
